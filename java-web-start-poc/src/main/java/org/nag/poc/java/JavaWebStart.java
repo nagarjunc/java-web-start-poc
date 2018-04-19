fa894ef6501fadea7238f2356fb6454febba3d2d
Nagarjun C <nagarjun@outlook.com>
2018-04-19 10:43:56 -0500

    First Change

--- java-web-start-poc/src/main/java/org/nag/poc/java/JavaWebStart.java
+++ java-web-start-poc/src/main/java/org/nag/poc/java/JavaWebStart.java
@@ -3,7 +3,7 @@ package org.nag.poc.java;
 public class JavaWebStart {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World GIT!");
 
 	}
 
