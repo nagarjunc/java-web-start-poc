579239d93de7d0b9c104c77c7f15e2d29a87d655
NAG <s510288@mail.nwmissouri.edu>
2021-12-03 10:42:42 -0600

    Create azureShellScript.sh

---
+++ azureShellScript.sh
@@ -0,0 +1,13 @@
+echo "Here we are going to fetch a cat fact"
+
+URL="https://cat-fact.herokuapp.com/facts/random"
+
+response=$(curl -s -w "%{http_code}" $URL)
+
+http_code=$(tail -n1 <<< "$response")  # get the last line
+content=$(sed '$ d' <<< "$response")   # get all but the last line which contains the status code
+
+echo "$http_code"
+echo "$content"
+
+echo "Cat fact fetched success"
