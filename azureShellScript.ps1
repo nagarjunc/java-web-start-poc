2baaa684ef7a8ae461721aad9e0c2f29df52017b
NAG <s510288@mail.nwmissouri.edu>
2021-12-03 10:40:12 -0600

    Update azureShellScript.ps1

--- azureShellScript.ps1
+++ azureShellScript.ps1
@@ -2,6 +2,9 @@ echo "Here we are going to fetch a cat fact"
 
 ls -ial
 
+curl.exe -Uri 'https://cat-fact.herokuapp.com/facts/random' | select-object StatusCode
+
+echo "-----------------------------------------------------------------------------------"
 $params = @{ 
  Uri = "https://cat-fact.herokuapp.com/facts/random"
  Method = "Get"
